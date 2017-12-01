807df59ed983a832347c9521035801347aef78d2
Raghu Betina <raghu@firstdraft.com>
2017-12-01 14:58:50 +0000

    Whitelist columns in Datatype for ActiveAdmin

--- app/admin/datatype.rb
+++ app/admin/datatype.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Datatype do
 
+ permit_params :datatype
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
