d47f01d41de5487b67094746d94dda43a689fc32
Raghu Betina <raghu@firstdraft.com>
2017-12-01 14:58:36 +0000

    Whitelist columns in Study for ActiveAdmin

--- app/admin/study.rb
+++ app/admin/study.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Study do
 
+ permit_params :user_id, :title, :datatype_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
