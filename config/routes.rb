5bec80a98c06238f113298d4df60ca8fbe0054c5
Raghu Betina <raghu@firstdraft.com>
2017-12-01 14:58:28 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "studies#index"
   # Routes for the Datatype resource:
   # CREATE
