11e76a952c154f9b9e84d906f36b5e99980ab912
Raghu Betina <raghu@firstdraft.com>
2017-12-01 14:58:50 +0000

    Only study user can access show page

--- app/controllers/studies_controller.rb
+++ app/controllers/studies_controller.rb
@@ -1,5 +1,5 @@
 class StudiesController < ApplicationController
-  before_action :current_user_must_be_study_user, :only => [:edit, :update, :destroy]
+  before_action :current_user_must_be_study_user, :only => [:show, :edit, :update, :destroy]
 
   def current_user_must_be_study_user
     study = Study.find(params[:id])
